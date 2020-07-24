601de5663614a2a455ac4aab33c1346246b5f2e1
happyVR961 <66651826+happyVR961@users.noreply.github.com>
2020-07-23 21:29:26 -0700

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,27 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.rope = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.rope);
+    }
+  
+    // Fly(){
+    //     this.chain.bodyA = null;
+    // }
+    display(){
+        
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
+        
+    }
+    
+}
